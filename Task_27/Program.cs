5643060322e5e131779fd87714a086c99f7bc409
Abramov Sergey <Kononovaele@yandex.ru>
2023-01-13 20:49:25 +0300

    make task_27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -5,4 +5,40 @@
 // 9012 -> 12
 //
 
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    int rest = 0;
+
+    while(true) {
+        rest = value % 10;
+        sum = sum + rest;
+        value = value / 10;
+        if(value == 0) {
+            break;
+        }
+    }
+
+    return sum;
+}
+
+void main()
+{
+
+    Console.WriteLine("\n-------Task-27 ------- \n");
+
+    Console.Write("Input digit: ");
+    String? strValue = Console.ReadLine();
+    int value = 0;
+    if( String.IsNullOrEmpty(strValue) == false)
+    {
+        value = Int32.Parse(strValue);
+    }
+
+    int sum = GetSumOfDigits(value);
+    Console.WriteLine($"For digit: {value}, Sum is: {sum}");
+}
+
+main();
+
 
